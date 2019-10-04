a1559be40fdb720dad541f386187008935b95880
Jamie McInnes <41684417+AcidzDev@users.noreply.github.com>
2019-10-04 22:23:09 +0100

    Update Arch-Patcher.py

--- Arch-Patcher.py
+++ Arch-Patcher.py
@@ -9,10 +9,15 @@ import shutil
 
 
 # Patch URL
-PatchURL = "https://clbin.com/VCiYJ"
+PatchURL = 'https://clbin.com/VCiYJ'
 
 # Linux location
 LinuxPath = 'linux/trunk/'
+
+# Increase MAKE Performance
+f = open("makeupdate.txt", "r")
+string = f.read()
+os.system("sudo "+ string)
 
 #Script Start
 print("This script will patch the current kernel for ArchLinux")
@@ -20,7 +25,7 @@ print("WARNING: Sometimes after patching the kernel you may have driver issues, 
 input ("Press ENTER to Begin....")
 
 # Install required packages
-os.system("pacman -Syu base-devel asp pacman-contrib")
+os.system("sudo pacman -Syu base-devel asp pacman-contrib")
 
 # Checkout linux package
 print("Obtaining the kernel")
@@ -33,7 +38,7 @@ os.chdir(LinuxPath)
 print(LinuxPath)
 
 # Download patch
-wget.download(PatchURL, LinuxPath)
+wget.download(PatchURL)
 time.sleep(0.5)
 
 # Change name of patch
@@ -43,35 +48,28 @@ shutil.move('VCiYJ', 'pci.patch')
 # Add pci.patch to PKGBUUILD
 os.system("sed -i '/linux.preset   # standard config files for mkinitcpio ramdisk/a    \  pci.patch      # PCI Header 127 Fix' PKGBUILD")
 
-# Increase MAKE Performance
-f = open("makeupdate.txt", "r")
-string = f.read()
-os.system(string)
-
 # Update Pacman NOMNOMNOM
-os.system("pacman -Syu")
+os.system("sudo pacman -Syu")
 time.sleep(0.5)
 
 # update PKGBUILD
 os.system("updpkgsums")
 
 # Compile ya boi
-print("Are you ready to compile the kernel\n")
+print("Are you ready to compile the kernel")
 input("Press ENTER to continue...")
 os.system("makepkg -s")
 time.sleep(0.5)
 
 # Install compiled
-os.system('clear')
 answer = None
 while answer not in ("yes", "no"):
     answer = input("Do you wish to install the kernel now?, Enter yes or no: ")
     if answer == "yes":
-         os.system("pacman -U linux-*")
+         os.system("sudo pacman -U *.tar.gz")
          print("\nPlease update your bootloader with the new kernel and reboot")
     elif answer == "no":
          print("To install the kernel, navigate to linux/trunk and run sudo pacman -U linux-*")
          exit()
     else:
         print("Please enter yes or no.")
-
